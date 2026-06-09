32a9c353b9b4b907aa5e4c2396b2d63043ba0ec0
mdamaan7861234 <mdamaan861234@gmail.com>
2026-06-09 18:27:22 +0530

    assignment

---
+++ day2 js/note.js
@@ -0,0 +1,198 @@
+// let userName = "mohammed";
+// console.log (userName);
+// let age = 35;
+// console.log (age);
+
+// firstName = "mohammed";
+// console.log (firstName);
+
+// secondPlace = "Amaan";
+// console.log (secondPlace);
+
+// user_Email = "mdamaan@gmail.com";
+// console.log (user_Email);
+
+// total_Price = 100;
+// console.log (total_Price);
+
+// $amount = 120;
+// console.log ($amount);
+
+
+// myVariable = "xyz";
+// console.log(myVariable);
+
+// let _privateData = "password";
+// console.log (_privateData);
+
+// let oldvalue = 12;
+// console.log (oldvalue);
+
+
+
+
+// let userName = "Alice";
+// console.log (userName);
+
+// let firstPrize = 1000;
+// console.log (firstPrize);
+
+// let total_Cost = 250;
+// console.log (total_Cost);
+
+// let userAge = 25;
+// console.log (userAge);
+
+// let emailAddress = "john@Example.com";
+// console.log(emailAddress);
+
+// let islogin = true;
+// console.log (islogin);
+
+// const numberValue = 3;
+// console.log(numberValue);
+
+// let studentMarks = 85;
+// console.log(studentMarks);
+
+// let $productPrice = 99.99;
+// console.log ($productPrice);
+
+
+// let israining = false;
+// console.log (israining);
+
+// let age = Number ;
+//  let withage = true;
+//  if(age >=18)
+// {
+//     withage = true;
+// } else{
+//     withage = false;
+//  }
+//  console.log (withage);
+
+// let isPasswordmatch = false;
+// console.log (isPasswordmatch);
+
+// let x = 10;
+// console.log(typeof x); // output is number
+
+// x = "Hello";
+// console.log(typeof x); //output is string
+
+// x = true;
+// console.log(typeof x); // output is boolean
+
+
+// let language = "javascript";
+// console.log (language[0])
+// console.log (language[4])
+// console.log (language[9])
+// console.log (language.length)
+
+// let message = "its a message for u";
+// console.log (message);
+// let message2 = 'its a sunny day';
+// console.log (message2);
+// let message3 = "hello";
+// console.log(message3);
+
+// let firstName = "john";
+// let lastName = "Doe";
+// let fullName = firstName + " " + lastName;
+// console.log (fullName);
+
+// let greeting = "hello john doe";
+// console.log (greeting);
+
+// let age = 25;
+// let info = `john is ${age} years old`;
+// console.log (info);
+
+// let name = "Alice";
+// let score = 95;
+// let message = "Student " + name + " scored " + score + " points.";
+
+// let userName = `student ${name} scored ${score} points`;
+// console.log (userName);
+
+
+// let product = "Laptop";
+// let price = 999;
+// let quantity = 2;
+// let total = price * quantity;
+// let receipt = "Item: " + product + ", Quantity: " + quantity + ", Total: $" + total;
+
+// let receip = `item ${product} ${quantity} Quantity Total ${total}`;
+
+// console.log (receip);
+
+// let cityName = "Paris";
+// let temperature = 25;
+// let weather = `${cityName} and the temperature of is ${temperature}`;
+// console.log (weather);
+
+// let hourOfDay = 14;
+// let userName = "Bob";
+// let create = `Good afternoon,${userName} it's ${hourOfDay}.00`;
+// console.log (create);
+
+//  let score;
+//  console.log(score); // undefined
+
+// let winner = null;
+// console.log(winner); //null
+
+// let user = { name: "Alice" };
+// console.log(user.age); //undefined
+
+// console.log(10 > 5); // _________true______
+// console.log(3 < 2); // _________flase______
+// console.log(5 >= 5); // _____________true__
+// console.log(8 <= 10); // __true_____________
+// console.log(7 != 7); // _____false__________
+// console.log(15 > 20); // _____true__________
+
+// let isage = 18;
+// console.log (18===18);
+
+// let temperature = 30;
+// console.log (30>25);
+
+// let score = 75;
+// console.log (70<60);
+
+// console.log(5 == 5); // ________true_______
+// console.log(5 === 5); // _______true________
+// console.log(5 == "5"); // ______true_______
+// console.log(5 === "5"); // ________flase_______
+// console.log(true == 1); // _____________true__
+// console.log(true === 1); // ___________flase____
+// console.log(0 == false); // __________true_____
+// console.log(0 === false); // _____false__________
+
+// let userInput = "10";
+// if(userInput === 10){
+//     console.log ("Input is 10")
+// }
+// console.log (userInput);
+
+// console.log('a' > 'A'); // ______true_________
+// console.log('b' < 'c'); // ________true_______
+// console.log('apple' < 'banana'); // __true______________
+// console.log('Z' < 'a'); // _________true_______
+// console.log("10" < "2"); // _________true_ because '1' is 49 and '2' is 50______
+
+
+// let str1 = "Hello";
+// let str2 = "Hello";
+// console.log (str1 === str2);
+
+// let userName = "Sarah";
+// let hour = 9; // 9 AM
+// let inputName = `Good morning ${userName}`;
+// console.log (inputName);
+
+
+
